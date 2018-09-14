1f9b881e7d29bc19b07ecb7b943b421b7a2afe19
vitor <vitor@BROLY>
2018-09-14 10:12:03 -0300

    classes padrão inicial

--- src/main/java/com/ApiStudy/Start.java
+++ src/main/java/com/ApiStudy/Start.java
@@ -1,9 +1,15 @@
 package com.ApiStudy;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan ("com.ApiStudy")
 public class Start {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SpringApplication.run(Start.class, args);
 
 	}
 
