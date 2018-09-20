0a3e84ffa446c4aa806b4be4b3e21355346e6108
vitor <vitor@BROLY>
2018-09-20 14:21:31 -0300

    ...

--- src/main/java/com/ApiStudy/Start.java
+++ src/main/java/com/ApiStudy/Start.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan ("com.ApiStudy")
 public class Start {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(Start.class, args);
 	
