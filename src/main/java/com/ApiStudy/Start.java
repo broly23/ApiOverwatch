5e1b6d8e9be6e92bd33566f9a5df6ea71cb9f622
taisemarques <taise.marques@hotmail.com>
2018-10-02 21:32:25 -0300

    add messages

--- src/main/java/com/ApiStudy/Start.java
+++ src/main/java/com/ApiStudy/Start.java
@@ -1,13 +1,13 @@
 package com.ApiStudy;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableCaching
 @SpringBootApplication
-@ComponentScan ("com.ApiStudy")
+@EnableRabbit
+@ComponentScan ({"com.ApiStudy"})
 public class Start {
 
 	public static void main(String[] args) {
